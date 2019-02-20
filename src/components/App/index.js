45ea31a5d5b03ac16b496fad96b81e9d9574b2f8
Josh Adesoba <josh.adesoba@stockport.gov.uk>
2019-02-20 15:59:36 +0000

    feature(TDKT): ReAdded page 3 route to App index

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,7 @@ import { getPageRoute } from '../../helpers/pagehelper'
 import { ErrorPage } from 'smbc-react-components'
 import TypeOfTest from '../Pages/1-TypeOfTest'
 import Resit from '../Pages/2-Resit'
-// import AboutYourself from '../Pages/3-AboutYourself'
+import AboutYourself from '../Pages/3-AboutYourself'
 
 class App extends Component {
     render() {
@@ -12,7 +12,7 @@ class App extends Component {
             <Switch>
                 <Route exact path={getPageRoute(1)} component={ TypeOfTest } />
                 <Route exact path={getPageRoute(2)} component={ Resit } />
-                {/* <Route exact path={getPageRoute(3)} component={ AboutYourself }/> */}
+                <Route exact path={getPageRoute(3)} component={ AboutYourself }/>
                 <Route exact path="/error" component={ ErrorPage } />
             </Switch>
         )
