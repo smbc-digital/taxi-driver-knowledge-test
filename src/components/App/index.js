5c86dc33b75b642945efc4b419c926f9e2885b9b
Josh Adesoba <josh.adesoba@stockport.gov.uk>
2019-02-20 13:21:31 +0000

    fix(taxi knowledge): fixed merge conflict

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import { getPageRoute } from '../../helpers/pagehelper'
 import { ErrorPage } from 'smbc-react-components'
 import TypeOfTest from '../Pages/1-TypeOfTest'
 import Resit from '../Pages/2-Resit'
+import AboutYourself from '../Pages/3-AboutYourself'
 
 class App extends Component {
     render() {
@@ -11,6 +12,7 @@ class App extends Component {
             <Switch>
                 <Route exact path={getPageRoute(1)} component={ TypeOfTest } />
                 <Route exact path={getPageRoute(2)} component={ Resit } />
+                <Route exact path={getPageRoute(3)} component={ AboutYourself }/>
                 <Route exact path="/error" component={ ErrorPage } />
             </Switch>
         )
