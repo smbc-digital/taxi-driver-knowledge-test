d6ab9d290246e220352e461712431d293a721534
Anthony H <Anthony-Haslett@outlook.com>
2019-06-10 17:34:26 +0100

    refactor(Success): changing time format to lower case

--- src/components/Pages/6-Success/index.js
+++ src/components/Pages/6-Success/index.js
@@ -25,7 +25,7 @@ export class Success extends Component {
 					<p className="h2">Thank you for booking to take the taxi driver knowledge test</p>
 				</section>
 				<section className="body-container">
-					<p>Your appointment to take the {testType} taxi driver knowledge test is at <b>{moment(testDate, 'YYYY-MM-DD[T]HH:mm:ss').format('h:mmA')}</b> on <b>{moment(testDate, 'YYYY-MM-DD[T]HH:mm:ss').format('dddd D MMMM YYYY')}</b> </p>
+					<p>Your appointment to take the {testType} taxi driver knowledge test is at <b>{moment(testDate, 'YYYY-MM-DD[T]HH:mm:ss').format('h:mma')}</b> on <b>{moment(testDate, 'YYYY-MM-DD[T]HH:mm:ss').format('dddd D MMMM YYYY')}</b> </p>
                     <p>We&#39;ve sent you a confirmation email including this information.</p>
 					<h2>What happens next</h2>
 					<p>
