a509ca632504372bd3f75518ee0c779606713672
Anthony H <Anthony-Haslett@outlook.com>
2019-07-11 10:59:43 +0100

    Anthony/Simon added a full stop tpp to 'bool your test'

--- src/components/Pages/4-SelectAppointment/index.js
+++ src/components/Pages/4-SelectAppointment/index.js
@@ -78,7 +78,7 @@ export class SelectAppointment extends Component {
 						<h1>{formHeader}</h1>
 						<AlertForm
 							level="information"
-							content="There are currently no online appointments available. Please email <a href='mailto:taxi.licensing@stockport.gov.uk'>taxi.licensing@stockport.gov.uk</a> to book your test"
+							content="There are currently no online appointments available. Please email <a href='mailto:taxi.licensing@stockport.gov.uk'>taxi.licensing@stockport.gov.uk</a> to book your test."
 						/>
 						<a className="button-primary" href="https://www.stockport.gov.uk">Return to homepage</a>
 					</form>
